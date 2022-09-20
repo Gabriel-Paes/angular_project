10a71d04b0be42c56aef7773ea414afffe5a1ba6
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-09-20 16:41:04 -0300

    Replace Welcome to Home

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PilotosComponent } from './pilotos.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
-  { path: '', component: PilotosComponent }
+  { path:'', component: HomeComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PilotosRoutingModule { }
+export class HomeRoutingModule { }
