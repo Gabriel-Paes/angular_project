e175c7d55730aeeb3e77dc00fd5b7dc9b1eba25f
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-10-06 11:44:57 -0300

    Add Menu-dropdown

--- src/app/pages/pilotos/pilotos.module.ts
+++ src/app/pages/pilotos/pilotos.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzTableModule } from 'ng-zorro-antd/table';
 
+import { DropdownComponent } from './dropdown/dropdown.component';
 import { PilotosRoutingModule } from './pilotos-routing.module';
 import { PilotosComponent } from './pilotos.component';
 import { TableComponent } from './table/table.component';
@@ -11,7 +13,8 @@ import { TableComponent } from './table/table.component';
 @NgModule({
   declarations: [
     PilotosComponent,
-    TableComponent
+    TableComponent,
+    DropdownComponent
   ],
   imports: [
     CommonModule,
@@ -19,6 +22,7 @@ import { TableComponent } from './table/table.component';
     NzTableModule,
     NzDividerModule,
     NzListModule,
+    NzDropDownModule
   ]
 })
 export class PilotosModule { }
